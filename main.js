f69169ac6bb139901d33b5bde69e8e062ed78df6
Vu Duc <vuduc1711@gmail.com>
2021-02-10 21:31:25 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const rotation = document.getElementById("rotation");
+const btn = document.getElementById('btn');
 function rotationDuration() {
-  var i = 1;
+  var i = 2;
   while (i <= 10) {
     (function (i) {
       setTimeout(function () {
@@ -9,4 +10,20 @@ function rotationDuration() {
     })(i++);
   }
 }
-rotationDuration();
+btn.addEventListener("click",(e)=>{
+    rotation.style.setProperty("animation-duration", `${1 * 500}ms`);
+    console.log(e);
+    // let x = e.clientX - e.target.offsetLeft;
+    // let y = e.clientY - e.target.offsetTop;
+    let x = e.layerX;
+    let y = e.layerY+20;
+    let ripples = document.createElement("span");
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px ";
+    btn.appendChild(ripples);
+    setTimeout(()=>{
+        ripples.remove();
+    },1000);
+    rotationDuration();
+});
+
