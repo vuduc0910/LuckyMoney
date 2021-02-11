a61de9dfb8d80fb98ec08ec42f42fd874c403818
Vu Duc <vuduc1711@gmail.com>
2021-02-11 11:12:06 +0700

    update

--- random.js
+++
@@ -1,9 +0,0 @@
-var array = [];
-for (i = 0; i < 100; i++) {
-    array.push(i);
-}
-console.log(array);
-Math.floor(Math.random()+100);
-console.log(Math.floor((Math.random()*99)+0));
-console.log(array.pop());
-console.log(array);
